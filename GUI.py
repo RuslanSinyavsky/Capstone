daa1c35b72782f82908985c290751353adfea3d8
mperl <mperlitch55@gmail.com>
2021-02-17 19:04:46 -0500

    Added unit options to the GUI, fixed the time loop

--- GUI.py
+++ GUI.py
@@ -1,44 +1,44 @@
 from tkinter import *
 import Main as main
 
-
 root = Tk()
 frame = Frame(root, height=300, width=380)
 frame.pack()
 
-#root2 = Tk()
-#frame2 = Frame(root2, height=270, width=410)
-#frame2.pack()
-
 #Labels
 enter_param_label = Label(root, text="Please set the following parameters")
 nb_pics_label = Label(root, text="Number of pictures: ")
-time_label = Label(root, text="Time interval (min): ")
-maxdropsize_label = Label(root, text="Max droplet size allowed (unit?): ")
+time_label = Label(root, text="Time interval: ")
+maxcellsize_label = Label(root, text="Max cell size allowed (unit?): ")
 mindropsize_label = Label(root, text="Min droplet size allowed (unit?): ")
-#light_warning_label = Label(root2, text="Please ensure that the light in the room is off", fg='blue')
-#lamp_warning_label = Label(root2, text="Warning! Turn on the TH4 power supply before connecting cables", fg='red')
 
 #Entries
 nb_pics_entry = Entry(root, bd=3, width=6)
 time_entry = Entry(root, bd=3, width=6)
-maxdrop_entry = Entry(root, bd=3, width=6)
+maxcell_entry = Entry(root, bd=3, width=6)
 mindrop_entry = Entry(root, bd=3, width=6)
 
+#Time units option Menu
+
+#creating a Tkinter variable
+tkvar = StringVar(root)
+
+#time unit options
+time_units = {'s', 'min', 'hr'}
+unit_list = OptionMenu(frame, tkvar, *time_units)
+
+def chooseUnit(*args):
+    print("Unit chosen:", tkvar.get())
+
+tkvar.trace('w', chooseUnit)
+
 #Buttons
-
 def Setting():
-
-    main.IncubationTime(int(nb_pics_entry.get()), int(time_entry.get()))
-    main.RunSetup(int(nb_pics_entry.get()), int(time_entry.get()), int(maxdrop_entry.get()))
-
-    #print("nb of pics entered: ", nb_pics_entry.get())
-    #print("time entered: ", time_entry.get())
-    #print("max size entered: ", maxdrop_entry.get())
-    #print("min size entered: ", mindrop_entry.get())
 
     def imagingStart():
         start_btn.config(state=DISABLED)
+        #main.IncubationTime(int(nb_pics_entry.get()), int(time_entry.get()), tkvar.get())
+        main.RunSetup(int(nb_pics_entry.get()), int(time_entry.get(), base=10), tkvar.get(), int(maxcell_entry.get()), int(mindrop_entry.get()))
 
     root2 = Tk()
     frame2 = Frame(root2, height=270, width=410)
@@ -57,12 +57,7 @@ def Setting():
     lamp_warning_label.place(x=20, y=40)
     root2.mainloop()
 
-#def imagingStart():
-#    start_btn.config(state=DISABLED)
-
 set_btn = Button(root, text="Set", width=5, command=Setting)
-
-#start_btn = Button(root2, text="Begin imaging", width=12, command=imagingStart)
 
 #Packing
 enter_param_label.pack()
@@ -74,17 +69,11 @@ nb_pics_label.place(x=40, y=40)
 time_label.pack()
 time_label.place(x=40, y=70)
 
-maxdropsize_label.pack()
-maxdropsize_label.place(x=40, y=100)
+maxcellsize_label.pack()
+maxcellsize_label.place(x=40, y=100)
 
 mindropsize_label.pack()
 mindropsize_label.place(x=40, y=130)
-
-#light_warning_label.pack()
-#light_warning_label.place(x=20, y=10)
-
-#lamp_warning_label.pack()
-#lamp_warning_label.place(x=20, y=40)
 
 nb_pics_entry.pack()
 nb_pics_entry.place(x=160, y=40)
@@ -92,8 +81,8 @@ nb_pics_entry.place(x=160, y=40)
 time_entry.pack()
 time_entry.place(x=160, y=70)
 
-maxdrop_entry.pack()
-maxdrop_entry.place(x=220, y=100)
+maxcell_entry.pack()
+maxcell_entry.place(x=220, y=100)
 
 mindrop_entry.pack()
 mindrop_entry.place(x=220, y=130)
@@ -101,7 +90,8 @@ mindrop_entry.place(x=220, y=130)
 set_btn.pack()
 set_btn.place(x=180, y=200)
 
-#start_btn.pack()
-#start_btn.place(x=160, y=170)
+unit_list.pack()
+unit_list.place(x=210, y=66)
+
 root.mainloop()
-#root2.mainloop()
+
--- Main.py
+++ Main.py
@@ -1,20 +1,45 @@
-#import GUI as gui
 import time
 import datetime
 from datetime import timedelta
 
-start_time = datetime.datetime.now()
-end_time = datetime
+#start_time = datetime.datetime.now()
+#end_time = datetime
 
-def IncubationTime(nb_pics, time) -> datetime:
+#def IncubationTime(nb_pics, time, unit) -> datetime:
 
-    end_time = start_time + timedelta(seconds=(nb_pics-1) * time)
-    print(start_time)
-    print("total time:", end_time)
-    return end_time
+#    if unit == 's':
+#        end_time = start_time + timedelta(seconds=(nb_pics-1) * time)
+#        print("unit received:", unit)
+#    if unit == 'min':
+#        end_time = start_time + timedelta(minutes=(nb_pics-1) * time)
+#        print("unit received:", unit)
+#    if unit == 'hrs':
+#        end_time = start_time + timedelta(hours=(nb_pics-1) * time)
+#        print("unit received:", unit)
 
-def RunSetup(nb_pics, time, max_size):
+#    print("start time:", start_time)
+#    print("total time:", end_time)
+    #return end_time
 
-    total_time = start_time + timedelta(seconds=(nb_pics-1) * time)
-    while datetime.datetime.now() < total_time:
-        print("test")
+def RunSetup(nb_pics, time, unit, max_size, min_size):
+
+    start_time = datetime.datetime.now()
+    if unit == 's':
+        end_time = start_time + timedelta(seconds=(nb_pics-1) * time)
+        print("unit received:", unit)
+    if unit == 'min':
+        end_time = start_time + timedelta(minutes=(nb_pics-1) * time)
+        print("unit received:", unit)
+    if unit == 'hrs':
+        end_time = start_time + timedelta(hours=(nb_pics-1) * time)
+        print("unit received:", unit)
+
+    print("start time:", start_time)
+    print("end time:", end_time)
+
+    #end_time = start_time + timedelta(seconds=(nb_pics-1) * time)
+    print(datetime.datetime.now())
+    while datetime.datetime.now() < end_time:
+        print("in time loop")
+
+
